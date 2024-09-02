b426158e94a312d4e136ab2dfe4996423acc1a0a
AltmanEA <altmanea@gmail.com>
2024-09-02 16:25:37 +0600

    1_1_3 Spot the mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-// 1_1_2 Fix the return statement 
+// 1_1_3 Spot the mistake
 /* 
-  Исправьте ошибку в операторе return
+  Исправьте ошибку в компоненте Profile.
+  Вспомните, как react отличает компоненты от обычных HTML-тегов.
 */
 
-export default function Profile() {
-  return
-    <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+function profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
 }
 
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <profile />
+      <profile />
+      <profile />
+    </section>
+  );
+}
