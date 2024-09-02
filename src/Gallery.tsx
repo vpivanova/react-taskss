ac864911ab2b1cbef5a02f8b59d9bcad6163fd25
AltmanEA <altmanea@gmail.com>
2024-09-02 17:07:15 +0600

    1_2_1 Split the components further

---
+++ src/Gallery.tsx
@@ -0,0 +1,21 @@
+// Move me to Profile.tsx!
+export function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+  
