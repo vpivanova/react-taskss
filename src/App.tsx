880ee975e8eeb2d0a1f111ffcf4557acbf33202f
EA <altmanea@gmail.com>
2024-09-03 08:42:59 +0600

    1_3_1 Convert some HTML to JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-// 1_2_1 Split the components further
+// 1_3_1 Convert some HTML to JSX
 /* 
-В настоящее время Gallery.js экспортирует и Profile и Gallery, что немного запутывает.
-
-Переместите компонент Profile в собственный Profile.tsx, а затем измените компонент App, чтобы он отображал и <Profile />, и <Gallery /> друг за другом.
-
-Вы можете использовать либо экспорт по умолчанию, либо именованный экспорт для Profile, но убедитесь, что вы используете соответствующий синтаксис импорта как в App.tsx, так и в Gallery.tsx!
-
+  Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
 */
 
-import Gallery, { Profile } from "./Gallery";
-
-// Move me to Profile.js!
-
-export default function App() {
+export default function Bio() {
   return (
-    <div>
-      <Profile />      
+    <div class="intro">
+      <h1>Welcome to my website!</h1>
     </div>
+    <p class="summary">
+      You can find my thoughts here.
+      <br><br>
+      <b>And <i>pictures</b></i> of scientists!
+    </p>
   );
 }
