12787dccb779a2c9a713556a7b0de5f4cddf0277
EA <altmanea@gmail.com>
2024-09-03 17:47:25 +0600

    1_6_2 Show the item importance with &&

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
-// 1_6_1 Show an icon for incomplete items with ? :
+// 1_6_2 Show the item importance with &&
 /*
-  Используйте условный оператор (cond ? a : b) для отображения ❌, если isPacked не является true.
+В этом примере каждый Item получает числовой параметр importance. Используйте оператор &&, чтобы отобразить "(Важность: X)" курсивом, но только для тех элементов, которые имеют ненулевую важность. В итоге ваш список предметов должен выглядеть следующим образом:
+
+- Космический скафандр (Важность: 9).
+- Шлем с золотым листом
+- Фотография Тама (Важность: 6)
+
+Не забудьте добавить пробел между двумя метками!
 */
 
-function Item({ name, isPacked }: { name: string; isPacked: boolean }) {
+function Item({ name, importance }: { name: string, importance: number }) {
   return (
-      <li className="item">
-          {name} {isPacked && '✔'}
-      </li>
+    <li className="item">
+      {name}
+    </li>
   );
 }
 
 export default function PackingList() {
   return (
-      <section>
-          <h1>Sally Ride's Packing List</h1>
-          <ul>
-              <Item isPacked={true} name="Space suit" />
-              <Item
-                  isPacked={true}
-                  name="Helmet with a golden leaf"
-              />
-              <Item
-                  isPacked={false}
-                  name="Photo of Tam"
-              />
-          </ul>
-      </section>
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          importance={9} 
+          name="Space suit" 
+        />
+        <Item 
+          importance={0} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          importance={6} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
   );
 }
