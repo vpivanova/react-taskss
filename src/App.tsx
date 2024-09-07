a9894c76f24ba41cff3348e6fd9432d7d1b94797
AltmanEA <altmanea@gmail.com>
2024-09-07 14:36:55 +0600

    2_1_1 Fix an event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
-// 1_8_3 Fix a broken story tray
+// 2_1_1 Fix an event handler
 /*
-  Генеральный директор вашей компании просит вас добавить "истории" в ваше приложение онлайн-часов, и вы не можете отказать. Вы написали компонент StoryTray, который принимает список stories, за которым следует заполнитель "Create Story".
-
-  Вы реализовали заполнитель "Create Story", поместив еще одну фальшивую историю в конец массива stories, который вы получаете в качестве пропса. Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
+Щелчок на этой кнопке должен переключить фон страницы между белым и черным. Однако при нажатии ничего не происходит. Исправьте проблему. (Не беспокойтесь о логике внутри handleClick - эта часть в порядке).
 */
 
-export interface Story {
-  id: string;
-  label: string;
-} 
-
-const stories: Story[] = [];
-export default function Wrapper(){
-  return <StoryTray stories={stories}/>;
-}
-
- function StoryTray({ stories }: { stories: Story[] }) {
-  stories.push({
-      id: 'create',
-      label: 'Create Story',
-  });
+export default function LightSwitch() {
+  function handleClick() {
+      let bodyStyle = document.body.style;
+      if (bodyStyle.backgroundColor === 'black') {
+          bodyStyle.backgroundColor = 'white';
+      } else {
+          bodyStyle.backgroundColor = 'black';
+      }
+  }
 
   return (
-      <ul>
-          {stories.map((story) => (
-              <li key={story.id}>{story.label}</li>
-          ))}
-      </ul>
+      <button onClick={handleClick()}>
+          Toggle the lights
+      </button>
   );
 }
