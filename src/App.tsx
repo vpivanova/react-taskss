382807ef2e9850fb494bdea63255c935bd1ccbe9
AltmanEA <altmanea@gmail.com>
2024-09-07 14:53:04 +0600

    2_1_2 Wire up the events

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-// 2_1_1 Fix an event handler
+// 2_1_2 Wire up the events
 /*
-Щелчок на этой кнопке должен переключить фон страницы между белым и черным. Однако при нажатии ничего не происходит. Исправьте проблему. (Не беспокойтесь о логике внутри handleClick - эта часть в порядке).
+  Этот компонент ColorSwitch отображает кнопку. Он должен менять цвет страницы. Подключите его к обработчику события onChangeColor, который он получает от родителя, чтобы щелчок по кнопке изменил цвет.
+
+  После того, как вы это сделаете, обратите внимание, что нажатие на кнопку также увеличивает счетчик нажатий на страницу. Ваш коллега, написавший родительский компонент, настаивает, что onChangeColor не увеличивает никаких счетчиков. Что еще может происходить? Исправьте это так, чтобы нажатие на кнопку только изменяло цвет и не увеличивало счетчик.
 */
 
-export default function LightSwitch() {
-  function handleClick() {
-      let bodyStyle = document.body.style;
-      if (bodyStyle.backgroundColor === 'black') {
-          bodyStyle.backgroundColor = 'white';
-      } else {
-          bodyStyle.backgroundColor = 'black';
-      }
+import { useState } from "react";
+
+function ColorSwitch({
+  onChangeColor
+}: {
+  onChangeColor: () => void
+}) {
+  return (
+    <button>
+      Change color
+    </button>
+  );
+}
+
+export default function App() {
+  const [clicks, setClicks] = useState(0)
+
+  function handleClickOutside() {
+    setClicks(c => c + 1);
+  }
+
+  function getRandomLightColor() {
+    let r = 150 + Math.round(100 * Math.random());
+    let g = 150 + Math.round(100 * Math.random());
+    let b = 150 + Math.round(100 * Math.random());
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  function handleChangeColor() {
+    let bodyStyle = document.body.style;
+    bodyStyle.backgroundColor = getRandomLightColor();
   }
 
   return (
-      <button onClick={handleClick()}>
-          Toggle the lights
-      </button>
+    <div style={{ width: '100%', height: '100%' }} onClick={handleClickOutside}>
+      <ColorSwitch onChangeColor={handleChangeColor} />
+      <br />
+      <br />
+      <h2>Clicks on the page: {clicks}</h2>
+    </div>
   );
 }
