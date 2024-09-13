4767858d484aed6819cf969314b926c8c4be0d77
AltmanEA <altmanea@gmail.com>
2024-09-13 15:58:51 +0600

    2_7_2 Remove an item from the shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// 2_7_1 Update an item in the shopping cart 
+// 2_7_2 Remove an item from the shopping cart 
 /*
-    Заполните логику handleIncreaseClick так, чтобы нажатие "+" увеличивало соответствующее число.
+    В этой корзине есть рабочая кнопка "+", но кнопка "-" ничего не делает. Вам нужно добавить обработчик события, чтобы нажатие на нее уменьшало count соответствующего товара. Если вы нажмете "-", когда счетчик будет равен 1, товар должен автоматически удалиться из корзины. Убедитесь, что он никогда не показывает 0.
 */
 
 import { useState } from 'react';
@@ -28,7 +28,20 @@ export default function ShoppingCart() {
         initialProducts
     );
 
-    function handleIncreaseClick(productId: number) { }
+    function handleIncreaseClick(productId: number) {
+        setProducts(
+            products.map((product) => {
+                if (product.id === productId) {
+                    return {
+                        ...product,
+                        count: product.count + 1,
+                    };
+                } else {
+                    return product;
+                }
+            })
+        );
+    }
 
     return (
         <ul>
@@ -42,6 +55,7 @@ export default function ShoppingCart() {
                     >
                         +
                     </button>
+                    <button>–</button>
                 </li>
             ))}
         </ul>
