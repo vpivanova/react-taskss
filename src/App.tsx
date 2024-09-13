cdce474b41775efe22b1f587e046fd43ccbb0e0f
AltmanEA <altmanea@gmail.com>
2024-09-13 13:36:36 +0600

    2_6_1 Fix incorrect state updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,58 @@
-// 2_5_2 Implement the state queue yourself  
+// 2_6_1 Fix incorrect state updates 
 /*
-    В этом задании вам предстоит реализовать крошечную часть React с нуля! Это не так сложно, как кажется.
+    В этой форме есть несколько ошибок. Несколько раз нажмите на кнопку, увеличивающую оценку. Заметьте, что он не увеличивается. Затем отредактируйте имя и фамилию и заметите, что оценка внезапно "подхватила" ваши изменения. Наконец, отредактируйте фамилию, и заметите, что оценка полностью исчезла.
 
-    Посмотрите результат работы программы. Обратите внимание, что в ней показаны четыре тестовых случая. Они соответствуют примерам, которые вы видели ранее на этой странице. Ваша задача — реализовать функцию getFinalState так, чтобы она возвращала правильный результат для каждого из этих случаев. Если вы реализуете функцию правильно, все четыре теста должны пройти.
-
-    Вы получите два аргумента: baseState — начальное состояние (например, 0), и queue — массив, содержащий смесь чисел (например, 5) и функций обновления (например, n => n + 1) в порядке их добавления.
-
-    Ваша задача — вернуть конечное состояние, точно такое же, как в тестах на странице с результатом работы программы!
+    Ваша задача — исправить все эти ошибки. Исправляя их, объясните, почему происходит каждая из них.
 */
-import { getFinalState } from './processQueue';
 
-export type Que = number | ((n: number) => number);
+import { useState } from 'react';
 
-function increment(n: number) {
-    return n + 1;
-}
-increment.toString = () => 'n => n+1';
+export default function Scoreboard() {
+    const [player, setPlayer] = useState({
+        firstName: 'Ranjani',
+        lastName: 'Shettar',
+        score: 10,
+    });
 
-export default function App() {
+    function handlePlusClick() {
+        player.score++;
+    }
+
+    function handleFirstNameChange(e: any) {
+        setPlayer({
+            ...player,
+            firstName: e.target.value,
+        });
+    }
+
+    function handleLastNameChange(e: any) {
+        setPlayer({
+            lastName: e.target.value,
+        } as any);
+    }
+
     return (
         <>
-            <TestCase
-                baseState={0}
-                queue={[1, 1, 1]}
-                expected={1}
-            />
-            <hr />
-            <TestCase
-                baseState={0}
-                queue={[increment, increment, increment]}
-                expected={3}
-            />
-            <hr />
-            <TestCase
-                baseState={0}
-                queue={[5, increment]}
-                expected={6}
-            />
-            <hr />
-            <TestCase
-                baseState={0}
-                queue={[5, increment, 42]}
-                expected={42}
-            />
-        </>
-    );
-}
-
-function TestCase(
-    { baseState, queue, expected }: {
-        baseState: number;
-        queue: Que[];
-        expected: number;
-    }) {
-    const actual = getFinalState(baseState, queue);
-    return (
-        <>
-            <p>
-                Base state: <b>{baseState}</b>
-            </p>
-            <p>
-                Queue: <b>[{queue.join(', ')}]</b>
-            </p>
-            <p>
-                Expected result: <b>{expected}</b>
-            </p>
-            <p
-                style={{
-                    color:
-                        actual === expected
-                            ? 'green'
-                            : 'red',
-                }}
-            >
-                Your result: <b>{actual}</b> (
-                {actual === expected ? 'correct' : 'wrong'})
-            </p>
+            <label>
+                Score: <b>{player.score}</b>{' '}
+                <button onClick={handlePlusClick}>
+                    +1
+                </button>
+            </label>
+            <label>
+                First name:
+                <input
+                    value={player.firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={player.lastName}
+                    onChange={handleLastNameChange}
+                />
+            </label>
         </>
     );
 }
