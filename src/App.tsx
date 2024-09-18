2b58ba8891de55c560c1acc82f06593769e5584f
AltmanEA <altmanea@gmail.com>
2024-09-18 09:27:35 +0600

    3_3_2 Filtering a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-// 3_3_1 Synced inputs 
+// 3_3_2 Filtering a list  
 /*
-  Эти два входа являются независимыми. Сделайте их синхронизированными: редактирование одного входа должно обновить другой вход с тем же текстом, и наоборот.
+  В этом примере SearchBar имеет собственное состояние query, которое управляет вводом текста. Его родительский компонент FilterableList отображает список элементов, но он не учитывает поисковый запрос.
+
+  Используйте функцию filterItems(foods, query) для фильтрации списка в соответствии с поисковым запросом. Чтобы проверить ваши изменения, проверьте, что ввод "s" в поле ввода отфильтровывает список до "Sushi", "Shish kebab" и "Dim sum".
+
+  Обратите внимание, что filterItems уже реализован и импортирован, поэтому вам не нужно писать его самостоятельно!
 */
 
 import { useState } from 'react';
+import { foods, filterItems, Item } from './data';
 
-export default function SyncedInputs() {
+export default function FilterableList() {
   return (
     <>
-      <Input label="First input" />
-      <Input label="Second input" />
+      <SearchBar />
+      <hr />
+      <List items={foods} />
     </>
   );
 }
 
-function Input({ label }: { label: string }) {
-  const [text, setText] = useState('');
+function SearchBar() {
+  const [query, setQuery] = useState('');
 
-  function handleChange(e) {
-    setText(e.target.value);
+  function handleChange(e: any) {
+    setQuery(e.target.value);
   }
 
   return (
     <label>
-      {label}
-      {' '}
+      Search:{' '}
       <input
-        value={text}
+        value={query}
         onChange={handleChange}
       />
     </label>
   );
 }
 
+function List({ items }: { items: Item[] }) {
+  return (
+    <table>
+      <tbody>
+        {items.map(food => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
