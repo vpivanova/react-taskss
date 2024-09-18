9464c2c28130f59cf443a831d5a84018d067f7ae
AltmanEA <altmanea@gmail.com>
2024-09-18 10:34:17 +0600

    3_4_1 Fix disappearing input text

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,40 @@
-// 3_3_2 Filtering a list  
+// 3_4_1 Fix disappearing input text 
 /*
-  В этом примере SearchBar имеет собственное состояние query, которое управляет вводом текста. Его родительский компонент FilterableList отображает список элементов, но он не учитывает поисковый запрос.
-
-  Используйте функцию filterItems(foods, query) для фильтрации списка в соответствии с поисковым запросом. Чтобы проверить ваши изменения, проверьте, что ввод "s" в поле ввода отфильтровывает список до "Sushi", "Shish kebab" и "Dim sum".
-
-  Обратите внимание, что filterItems уже реализован и импортирован, поэтому вам не нужно писать его самостоятельно!
+  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
 */
 
 import { useState } from 'react';
-import { foods, filterItems, Item } from './data';
 
-export default function FilterableList() {
-  return (
-    <>
-      <SearchBar />
-      <hr />
-      <List items={foods} />
-    </>
-  );
-}
-
-function SearchBar() {
-  const [query, setQuery] = useState('');
-
-  function handleChange(e: any) {
-    setQuery(e.target.value);
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  if (showHint) {
+    return (
+      <div>
+        <p><i>Hint: Your favorite city?</i></p>
+        <Form />
+        <button onClick={() => {
+          setShowHint(false);
+        }}>Hide hint</button>
+      </div>
+    );
   }
-
   return (
-    <label>
-      Search:{' '}
-      <input
-        value={query}
-        onChange={handleChange}
-      />
-    </label>
+    <div>
+      <Form />
+      <button onClick={() => {
+        setShowHint(true);
+      }}>Show hint</button>
+    </div>
   );
 }
 
-function List({ items }: { items: Item[] }) {
+function Form() {
+  const [text, setText] = useState('');
   return (
-    <table>
-      <tbody>
-        {items.map(food => (
-          <tr key={food.id}>
-            <td>{food.name}</td>
-            <td>{food.description}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <textarea
+      value={text}
+      onChange={e => setText(e.target.value)}
+    />
   );
 }
+
