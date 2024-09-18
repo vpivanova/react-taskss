d8441fcf8d51f87143ea4100a6f8f7657d99b342
AltmanEA <altmanea@gmail.com>
2024-09-18 10:38:26 +0600

    3_4_2 Swap two form fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,56 @@
-// 3_4_1 Fix disappearing input text 
+// 3_4_2 Swap two form fields
 /*
-  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
+  Эта форма позволяет вводить имя и фамилию. В ней также есть флажок, контролирующий, какое поле будет первым. Если установить флажок, поле "Фамилия" появится перед полем "Имя".
+
+  Это почти работает, но есть ошибка. Если вы заполните поле "Имя" и установите флажок, текст останется в первом поле (теперь это "Фамилия"). Исправьте это так, чтобы при изменении порядка ввода текст также перемещался.
 */
 
 import { useState } from 'react';
 
 export default function App() {
-  const [showHint, setShowHint] = useState(false);
-  if (showHint) {
+  const [reverse, setReverse] = useState(false);
+  let checkbox = (
+    <label>
+      <input
+        type="checkbox"
+        checked={reverse}
+        onChange={e => setReverse(e.target.checked)}
+      />
+      Reverse order
+    </label>
+  );
+  if (reverse) {
     return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
+      <>
+        <Field label="Last name" /> 
+        <Field label="First name" />
+        {checkbox}
+      </>
     );
+  } else {
+    return (
+      <>
+        <Field label="First name" /> 
+        <Field label="Last name" />
+        {checkbox}
+      </>
+    );    
   }
+}
+
+function Field({ label }: { label: string }) {
+  const [text, setText] = useState('');
   return (
-    <div>
-      <Form />
-      <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
-    </div>
+    <label>
+      {label}:{' '}
+      <input
+        type="text"
+        value={text}
+        placeholder={label}
+        onChange={e => setText(e.target.value)}
+      />
+    </label>
   );
 }
 
-function Form() {
-  const [text, setText] = useState('');
-  return (
-    <textarea
-      value={text}
-      onChange={e => setText(e.target.value)}
-    />
-  );
-}
 
