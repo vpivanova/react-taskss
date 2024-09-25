904c3fde5781a979928feb6731982887880cc5d1
AltmanEvgenii <AltmanEA@gmail.com>
2024-09-25 12:55:43 +0600

    4_1_3  Fix debouncing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-// 4_1_2  Fix a component failing to re-render
+// 4_1_3  Fix debouncing 
 /*
-  Эта кнопка должна переключаться между отображением "Вкл" и "Выкл". Однако она всегда показывает "Выкл". Что не так с этим кодом? Исправьте это.
+  В этом примере все обработчики нажатия на кнопку выполняют "антидребезг". Чтобы увидеть, что это значит, нажмите на одну из кнопок. Обратите внимание, что через секунду появится сообщение. Если вы нажмете кнопку в ожидании сообщения, таймер сбросится. Таким образом, если вы будете нажимать одну и ту же кнопку много раз, сообщение появится только через секунду после того, как вы перестанете нажимать. Антидребезг позволяет отложить выполнение какого-либо действия до тех пор, пока пользователь не "перестанет делать что-то".
+
+  Этот пример работает, но не совсем так, как было задумано. Кнопки не являются независимыми. Чтобы увидеть проблему, нажмите на одну из кнопок, а затем сразу же нажмите на другую. Можно было бы ожидать, что после задержки вы увидите сообщения обеих кнопок. Но отображается только сообщение последней кнопки. Сообщение первой кнопки теряется.
+
+  Почему кнопки мешают друг другу? Найдите и устраните проблему.
 */
-import { useRef } from 'react';
 
-export default function Toggle() {
-  const isOnRef = useRef(false);
+let timeoutID: number | undefined;
 
+function DebouncedButton({ onClick, children }: {
+  onClick: () => void, children: React.ReactNode
+}) {
   return (
     <button onClick={() => {
-      isOnRef.current = !isOnRef.current;
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        onClick();
+      }, 1000);
     }}>
-      {isOnRef.current ? 'Вкл' : 'Выкл'}
+      {children}
     </button>
   );
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <DebouncedButton
+        onClick={() => alert('Spaceship launched!')}
+      >
+        Launch the spaceship
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('Soup boiled!')}
+      >
+        Boil the soup
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('Lullaby sung!')}
+      >
+        Sing a lullaby
+      </DebouncedButton>
+    </>
+  )
 }
