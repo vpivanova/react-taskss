8c368c9abe91b4a5195c728e85f5b88e84f97382
AltmanEvgenii <AltmanEA@gmail.com>
2024-09-25 15:29:42 +0600

    4_2_3 Scrolling an image carouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,61 @@
-// 4_2_2 Focus the search field
+// 4_2_3 Scrolling an image carouse
 /*
-  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
+  Эта карусель изображений имеет кнопку "Next", которая переключает активное изображение. Заставьте галерею прокручиваться горизонтально до активного изображения по щелчку. Для этого нужно вызвать scrollIntoView() на DOM-узле активного изображения:
+  
+  node.scrollIntoView({
+    behavior: 'smooth',
+    block: 'nearest',
+    inline: 'center',
+  });
 */
+import { useState } from 'react';
 
-export default function Page() {
+export default function CatFriends() {
+  const [index, setIndex] = useState(0);
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={() => {
+          if (index < catList.length - 1) {
+            setIndex(index + 1);
+          } else {
+            setIndex(0);
+          }
+        }}>
+          Next
+        </button>
       </nav>
-      <input
-        placeholder="Looking for something?"
-      />
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li key={cat.id}>
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
+
+type PlaceType = {
+  id: number;
+  imageUrl: string;
+}
+
+const catList: PlaceType[] = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://loremflickr.com/320/240?cat=' + i
+  });
+}
+
