8285f416e6bb9461e692a8bf82ca7335728c84df
AltmanEvgenii <AltmanEA@gmail.com>
2024-09-25 15:09:59 +0600

    4_2_2 Focus the search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-// 4_2_1 Play and pause the video
+// 4_2_2 Focus the search field
 /*
-  В этом примере кнопка переключает переменную состояния для перехода между воспроизведением и паузой. Однако для того, чтобы действительно воспроизвести или поставить видео на паузу, переключения состояния недостаточно. Вам также необходимо вызвать play() и pause() на элементе DOM для <video>. Добавьте к нему ссылку и заставьте кнопку работать.
-
-  Для решения дополнительной задачи синхронизируйте кнопку "Play" с тем, воспроизводится ли видео, даже если пользователь щелкает правой кнопкой мыши на видео и воспроизводит его с помощью встроенных элементов управления мультимедиа браузера. Для этого вам может понадобиться прослушать onPlay и onPause на видео.
+  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
 */
 
-import { useState, useRef } from 'react';
-
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function handleClick() {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
-  }
-
+export default function Page() {
   return (
     <>
-      <button onClick={handleClick}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <video width="250">
-        <source
-          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <nav>
+        <button>Search</button>
+      </nav>
+      <input
+        placeholder="Looking for something?"
+      />
     </>
-  )
+  );
 }
