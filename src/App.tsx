96b750c8e11002f2a7b50dcba6d6b2819f14992a
AltmanEvgenii <AltmanEA@gmail.com>
2024-09-26 15:23:34 +0600

    4_3_4 Fix fetching inside an Effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-// 4_3_3 Fix an interval that fires twice
+// 4_3_4 Fix fetching inside an Effect
 /*
-  Этот компонент Counter отображает счетчик, который должен увеличиваться каждую секунду. При монтировании он вызывает setInterval. Это заставляет функцию onTick выполняться каждую секунду. Функция onTick увеличивает счетчик.
+  Этот компонент показывает биографию для выбранного человека. Он загружает биографию, вызывая асинхронную функцию fetchBio(person) при монтировании и при каждом изменении person. Эта асинхронная функция возвращает Promise, который в конечном итоге разрешается в строку. Когда выборка завершена, вызывается setBio для отображения этой строки в поле выбора.
 
-  Однако вместо того, чтобы увеличиваться один раз в секунду, она увеличивается дважды. Почему так происходит? Найдите причину ошибки и исправьте ее.
+  В этом коде есть ошибка. Начните с выбора "Алисы". Затем выберите "Боб" и сразу после этого выберите "Тейлор". Если вы сделаете это достаточно быстро, вы заметите эту ошибку: Тейлор выбран, но в абзаце ниже написано "Это биография Боба".
+
+  Почему так происходит? Исправьте ошибку внутри этого Эффекта.
 */
-import { useState } from 'react';
-import Counter from './Counter.js';
 
-export default function Form() {
-  const [show, setShow] = useState(false);
+import { useState, useEffect } from 'react';
+import { fetchBio } from './api.js';
+
+export default function Page() {
+  const [person, setPerson] = useState('Alice');
+  const [bio, setBio] = useState<string | null> (null);
+
+  useEffect(() => {
+    setBio(null);
+    fetchBio(person).then(result => {
+      setBio(result);
+    });
+  }, [person]);
+
   return (
     <>
-      <button onClick={() => setShow(s => !s)}>{show ? 'Hide' : 'Show'} counter</button>
-      <br />
+      <select value={person} onChange={e => {
+        setPerson(e.target.value);
+      }}>
+        <option value="Alice">Alice</option>
+        <option value="Bob">Bob</option>
+        <option value="Taylor">Taylor</option>
+      </select>
       <hr />
-      {show && <Counter />}
+      <p><i>{bio ?? 'Loading...'}</i></p>
     </>
   );
 }
