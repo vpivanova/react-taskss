2218d643ae5f3bacf1213c44bd76d1418df8390e
Veronika <Nik_name27@mail.ru>
2024-09-26 15:06:34 +0600

    1_14

--- src/App.tsx
+++ src/App.tsx
@@ -2,3 +2,13 @@
 /* 
 Напишите компонент с нуля. Вы можете дать ему любое допустимое имя и вернуть любую разметку. Если у вас нет идей, вы можете написать компонент Congratulations, который показывает <h1>Хорошая работа!</h1>. Не забудьте экспортировать это!
 */
+export default function Cat(){
+  return (
+    <section>
+      <img
+       src ="https://oclo.ru/wp-content/uploads/2023/12/pro-vykhodnye-smeshnye-1.webp"
+      />
+      <h1>Ты заслужил отдых!</h1>
+    </section>
+  );
+}
