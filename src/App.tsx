efa974ebae6a857b05449977a8e7dc354cb578ee
AltmanEvgenii <AltmanEA@gmail.com>
2024-10-01 09:19:26 +0600

    4_8_5  Implement a staggering movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-// 4_8_3 Extract useInterval out of useCounter
+// 4_8_5  Implement a staggering movement
 /*
-  В настоящее время ваш хук useCounter делает две вещи. Он устанавливает интервал, а также увеличивает переменную состояния при каждом тике интервала. Выделите логику, которая устанавливает интервал, в отдельный хук под названием useInterval. Он должен принимать два аргумента: обратный вызов onTick и delay. После этого изменения ваша реализация useCounter должна выглядеть следующим образом:
+  В этом примере хук usePointerPosition() отслеживает текущую позицию указателя. Попробуйте переместить курсор или палец по области предварительного просмотра и увидите, как красная точка следует за вашим движением. Ее положение сохраняется в переменной pos1.
 
-  export function useCounter(delay) {
-    const [count, setCount] = useState(0);
-    useInterval(() => {
-        setCount((c) => c + 1);
-    }, delay);
-    return count;
-  }
+  На самом деле, в данный момент отображается пять (!) различных красных точек. Вы не видите их, потому что в настоящее время все они отображаются в одном и том же положении. Это то, что вам нужно исправить. Вместо этого вы хотите реализовать "ступенчатое" движение: каждая точка должна "следовать" по пути предыдущей точки. Например, если вы быстро перемещаете курсор, первая точка должна следовать за ним немедленно, вторая точка должна следовать за первой с небольшой задержкой, третья точка должна следовать за второй и так далее.
+
+  Вам необходимо реализовать пользовательский хук useDelayedValue. Его текущая реализация возвращает предоставленное ему value. Вместо этого вы хотите возвращать значение, полученное от delay миллисекунды назад. Для этого вам может понадобиться некоторое состояние и Эффект.
+
+  После реализации useDelayedValue, вы должны увидеть, как точки движутся друг за другом.
 */
 
-import { useState } from 'react';
-import { useCounter } from './useCounter.js';
+import { usePointerPosition } from './usePointerPosition.js';
 
-export default function Counter() {
-  const count = useCounter(1000);
-  return <h1>Seconds passed: {count}</h1>;
+type Position = { x: number, y: number };
+
+function useDelayedValue(value: Position, delay: number) {
+  // TODO: Implement this Hook
+  return value;
 }
+
+export default function Canvas() {
+  const pos1 = usePointerPosition();
+  const pos2 = useDelayedValue(pos1, 100);
+  const pos3 = useDelayedValue(pos2, 200);
+  const pos4 = useDelayedValue(pos3, 100);
+  const pos5 = useDelayedValue(pos3, 50);
+  return (
+    <>
+      <Dot position={pos1} opacity={1} />
+      <Dot position={pos2} opacity={0.8} />
+      <Dot position={pos3} opacity={0.6} />
+      <Dot position={pos4} opacity={0.4} />
+      <Dot position={pos5} opacity={0.2} />
+    </>
+  );
+}
+
+function Dot(
+  { position, opacity }:
+    { position: Position, opacity: number }
+) {
+  return (
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'pink',
+      borderRadius: '50%',
+      opacity,
+      transform: `translate(${position.x}px, ${position.y}px)`,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+    }} />
+  );
+}
+
--- src/useInterval.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect } from "react";
-
-export function useInterval(callback: () => void, delay: number) {
-    // Тут нужно реализовать хук
-}
